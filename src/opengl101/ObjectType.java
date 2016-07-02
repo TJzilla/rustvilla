1c7945b2de1ea58b0ae8df58e18b8c3ec9cad982
Matt McNamara <matthewmac12@yahoo.com>
2016-07-02 03:21:42 -0700

    added an enum to represent different object types in additon to a GameObject class to represents game objects in the game

---
+++ src/opengl101/ObjectType.java
@@ -0,0 +1,7 @@
+package opengl101;
+
+public enum ObjectType
+{
+	// create enumerators for the different types of objects. Ghost objects will refer to any object which is invisible or has no visible asset associate with it
+	CHARACTER, TERRAIN, COLLIDABLE, GHOST, SCRIPT
+}
