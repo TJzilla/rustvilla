7aa1d6d36f87742c0fc88aed857c8f9843e2cc10
russeree <reese.russell@ymail.com>
2016-07-02 05:01:04 -0700

    Now Generates Colors

--- src/opengl101/Game.java
+++ src/opengl101/Game.java
@@ -1,7 +1,7 @@
 package opengl101;
 
-import com.jogamp.opengl.glu.*;
 import com.jogamp.opengl.GL3;
+import com.jogamp.opengl.util.FPSAnimator;
 import com.jogamp.opengl.GLAutoDrawable;
 import com.jogamp.opengl.GLCapabilities;
 import com.jogamp.opengl.GLEventListener;
@@ -16,11 +16,13 @@ public class Game extends JFrame implements GLEventListener
 	// define height and width of our window
 	private int win_width = 800;
 	private int win_height = 600;
+	private float r,b,g;
+
 	
 	public Game()
 	{
 		// create a swing window with the title "Minimal OpenGL"
-		super("Minimal OpenGL");
+		super("Rustvilla");
 		
 		// set the OpenGL profile to use
 		GLProfile profile = GLProfile.get(GLProfile.GL3);
@@ -28,6 +30,9 @@ public class Game extends JFrame implements GLEventListener
 		
 		// create a new canvas
 		GLCanvas canvas = new GLCanvas(capabilities);
+		FPSAnimator animator = new FPSAnimator(canvas, 60);
+		animator.start();
+		
 		// add an event listener
 		canvas.addGLEventListener(this);
 		
@@ -40,9 +45,12 @@ public class Game extends JFrame implements GLEventListener
 		this.setLocationRelativeTo(null);
 		this.setDefaultCloseOperation(JFrame.EXIT_ON_CLOSE);
 		this.setVisible(true);
-		this.setResizable(false);
+		this.setResizable(true);
 		
 		canvas.requestFocusInWindow();
+		r = 1;
+		b = 1;
+		g = 1;
 	}
 
 	// initializes an OGL drawable class and sets the background color
@@ -50,10 +58,29 @@ public class Game extends JFrame implements GLEventListener
 	{
 		GL3 gl = drawable.getGL().getGL3();
 		gl.glClearColor(0.392f, 0.584f, 0.929f, 1.0f);
+
 	}
 	
 	public void play()
-	{	
+	{
+		for (;;){
+			try 
+			{
+				Thread.sleep(0L);    // one second
+			}
+			catch (Exception e) {}  
+			b = (float) (b - 0.01);
+			r = (float) (r - 0.02);
+			g = (float) (g - 0.03);
+			
+			if((r < 0) || (b < 0) || (g < 0))
+			{
+				b = (float) (1 - 0.00);
+				r = (float) (1 - 0.00);
+				g = (float) (1 - 0.00);
+			}
+		}
+		
 	}
 	
 	// calls the OGL backend and requests to draw the window contents
@@ -61,7 +88,8 @@ public class Game extends JFrame implements GLEventListener
 	public void display(GLAutoDrawable drawable)
 	{
 		GL3 gl = drawable.getGL().getGL3();
-		gl.glClear(GL3.GL_COLOR_BUFFER_BIT | GL3.GL_DEPTH_BUFFER_BIT);
+		gl.glClear(GL3.GL_COLOR_BUFFER_BIT | GL3.GL_DEPTH_BUFFER_BIT);	
+		gl.glClearColor(r, g, b, 1.0f);
 		// call draw code here
 		gl.glFlush();
 	}
