24e986b83ca71c7f62e50cf8409eea3f492143fe
russeree <reese.russell@ymail.com>
2016-07-02 02:54:50 -0700

    Drawing Class Init

---
+++ src/opengl101/DrawImgBuffer.java
@@ -0,0 +1,10 @@
+package opengl101;
+
+import java.awt.image.*;
+import java.nio.ByteBuffer;
+
+public class DrawImgBuffer {
+	// Draw a bitmap to the window
+	public static ByteBuffer _32BitImages[] = new ByteBuffer [32];
+	
+}// End Class
