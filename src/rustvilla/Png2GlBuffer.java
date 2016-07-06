2a7149e27731300cc4d3a7b274d6a2a9dcdcb56f
russeree <reese.russell@ymail.com>
2016-07-06 03:11:02 -0700

    Added a function to find the scaling value for an object as a portion of the screen

--- src/rustvilla/Png2GlBuffer.java
+++ src/rustvilla/Png2GlBuffer.java
@@ -7,8 +7,8 @@ public class Png2GlBuffer {
 	// Storage of the 
 	public float obj_min_x, obj_min_y;
 	// Ratio between the screen width and the image width;
-	public float obj_2_screen_rato;
-	private float inernal_scalar;
+	private float internal_scalar;
+	private float img_scalar;
 	
 	// Asset Identification and Control
 	public String img_location;
@@ -17,4 +17,23 @@ public class Png2GlBuffer {
 	
 	// Store the vertices for the rectangle here; 
 	public float[] vertices = new float[4];
+	
+	/**
+	 * Will Create a Scalar for the image that when the image
+	 * width is multiplier is then becomes the internal scalar value as a percent of the 
+	 * total on real estate 
+	 */
+	public void image_width_scale()
+	{
+		// (2/Screen Width)/(Scale Percent * Image_width)
+		this.img_scalar = ((2/this.win_width)/(this.internal_scalar*this.img_width));
+	}
 }
+
+/*
+	// Implement this sometime
+	VBOSupported = gl.isFunctionAvailable("glGenBuffers") &&
+	gl.isFunctionAvailable("glBindBuffer") &&
+	gl.isFunctionAvailable("glBufferData") &&
+	gl.isFunctionAvailable("glDeleteBuffers");
+*/
