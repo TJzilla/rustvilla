a968966f6ba631be7e09739285dae4a4204c7948
TJzilla <tjmitch14@gmail.com>
2016-07-20 09:54:59 -0600

    inital libgdx commit

--- src/rustvilla/ObjectType.java
+++
@@ -1,7 +0,0 @@
-package rustvilla;
-
-public enum ObjectType
-{
-	// create enumerators for the different types of objects. Ghost objects will refer to any object which is invisible or has no visible asset associate with it
-	CHARACTER, TERRAIN, COLLIDABLE, GHOST, SCRIPT
-}
